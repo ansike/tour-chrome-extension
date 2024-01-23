e76ce3b1ca0c69cc559f3b7ceec6e655fe0a2ff2
ansike <984315765@qq.com>
2024-01-24 00:00:32 +0800

    feat: init

--- popup.tsx
+++ popup.tsx
@@ -1,24 +1,27 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function IndexPopup() {
   const [data, setData] = useState("")
+
+  useEffect(() => {
+    console.log(document.title)
+    console.log(window)
+
+    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+      if (changeInfo.status === 'complete') {
+        console.log('页面加载完成:', tab.url);
+      }
+    });
+  }, [])
 
   return (
     <div
       style={{
         padding: 16
       }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+      hello world
+      <br />
+      {document.title}
     </div>
   )
 }
